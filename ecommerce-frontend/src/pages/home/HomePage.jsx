69d9317e25a046ded74a516781c225d1a365bfc0
ChayankBhargava <chayankbhargava04@gmail.com>
2026-07-15 16:36:00 +0530

    use async await

--- ecommerce-frontend/src/pages/home/HomePage.jsx
+++ ecommerce-frontend/src/pages/home/HomePage.jsx
@@ -8,20 +8,17 @@ import '../../components/header.css'
 export function HomePage({ cart }) {
     const [products, setProducts] = useState([])
     useEffect(() => {
-        axios.get('/api/products').then((response) => {
+        const getHomeData=async ()=>{
+            const response=await axios.get('/api/products')
             setProducts(response.data)
-        })
-
+        }
+        getHomeData()
     }, [])
 
     return (
         <>
             <title>Ecommerce Project</title>
-
             <Header cart={cart} />
-
-            
-
             <div className="home-page">
                 <ProductsGrid products={products} />
             </div>
